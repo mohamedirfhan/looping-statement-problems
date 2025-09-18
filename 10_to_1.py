6f7480bbefcc6b8bf569cf820dace61ce4a5b5ee
Mohamed Irfhan M <irfhandon7755@gmail.com>
2025-09-18 07:13:49 +0530

    uploaded some basic problems

---
+++ 10_to_1.py
@@ -0,0 +1,7 @@
+a=10
+for a in range (10,0,-1):
+    print(a)
+i=1
+while i>=10:
+    print(i)
+    i-=1
---
+++ sum_of_n_numbers.py
@@ -0,0 +1,6 @@
+n=10
+total=0
+for i in range(1, n+1):
+    print(i)
+    total+=i
+    print(total)
